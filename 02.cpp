ec6860682a67593cdd41672c52322e40b9876049
wulu <wulu90@outlook.com>
2025-02-26 16:39:26 +0800

    2-2

--- 02.cpp
+++ 02.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -26,7 +28,38 @@ void part1() {
     println("{}", checksum);
 }
 
+void part2() {
+    ifstream input("input/input02");
+    vector<int> row_vec;
+    int num        = 0;
+    int result_sum = 0;
+    while (input >> num) {
+        row_vec.push_back(num);
+
+        if (input.peek() == '\r' || input.peek() == '\n') {
+            sort(row_vec.begin(), row_vec.end(), greater<int>());
+            bool divide_evenly = false;
+            for (size_t i = 0; i < row_vec.size() - 1; ++i) {
+                for (size_t j = i + 1; j < row_vec.size(); ++j) {
+                    if (row_vec[i] % row_vec[j] == 0) {
+                        result_sum += row_vec[i] / row_vec[j];
+                        divide_evenly = true;
+                        break;
+                    }
+                }
+                if (divide_evenly) {
+                    break;
+                }
+            }
+            row_vec.clear();
+        }
+    }
+
+    println("{}", result_sum);
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
