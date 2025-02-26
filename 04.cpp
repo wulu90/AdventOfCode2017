46bf01a525088ef4fd5c9e4b46a12159e7930ba2
wulu <wulu90@outlook.com>
2025-02-26 21:51:40 +0800

    4-1

--- 04.cpp
+++ 04.cpp
@@ -1,9 +1,11 @@
+#include <algorithm>
 #include <fstream>
 #include <print>
 #include <ranges>
 #include <set>
 #include <string>
 #include <string_view>
+#include <vector>
 
 using namespace std;
 
@@ -20,19 +22,42 @@ bool valid(const string& str) {
     return valid;
 }
 
-void part1() {
+bool valid2(const string& str) {
+    vector<string_view> svs;
+    bool valid = true;
+    for (auto sv : str | views::split(' ')) {
+        for (auto sv_prev : svs) {
+            if (is_permutation(sv.begin(), sv.end(), sv_prev.begin(), sv_prev.end())) {
+                valid = false;
+                break;
+            }
+        }
+        if (!valid) {
+            break;
+        }
+        svs.push_back(string_view(sv));
+    }
+    return valid;
+}
+
+void part1_2() {
     ifstream input("input/input04");
-    uint32_t valid_count = 0;
+    uint32_t valid_count  = 0;
+    uint32_t valid2_count = 0;
     for (string line; getline(input, line);) {
         if (valid(line)) {
             ++valid_count;
         }
+        if (valid2(line)) {
+            ++valid2_count;
+        }
     }
 
     println("{}", valid_count);
+    println("{}", valid2_count);
 }
 
 int main() {
-    part1();
+    part1_2();
     return 0;
 }
