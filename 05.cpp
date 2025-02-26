ebd9faf1a8fd612e26421add6b484fcec2ffe83c
wulu <wulu90@outlook.com>
2025-02-26 23:00:53 +0800

    5-1

---
+++ 05.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void part1() {
+    ifstream input("input/input05");
+    vector<int> off_vec;
+    for (int num; input >> num;) {
+        off_vec.push_back(num);
+    }
+
+    size_t i    = 0;
+    size_t step = 0;
+    while (i < off_vec.size()) {
+        auto tmp = i;
+        i += off_vec[i];
+        ++off_vec[tmp];
+        ++step;
+    }
+    println("{}", step);
+}
+
+int main() {
+    part1();
+    return 0;
+}
