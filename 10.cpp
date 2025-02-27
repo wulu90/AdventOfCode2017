06d9f7af0e744dd82a95bbfd325c5c8c19f40d53
wulu <wulu90@outlook.com>
2025-02-27 17:48:16 +0800

    10-2

--- 10.cpp
+++ 10.cpp
@@ -41,7 +41,53 @@ void part1() {
     println("{}", numlist[0] * numlist[1]);
 }
 
+void part2() {
+    ifstream input("input/input10");
+    vector<int> len_vec;
+
+    for (char c = input.get(); c != '\r' && c != '\n'; c = input.get()) {
+        len_vec.push_back(c);
+    }
+
+    len_vec.push_back(17);
+    len_vec.push_back(31);
+    len_vec.push_back(73);
+    len_vec.push_back(47);
+    len_vec.push_back(23);
+
+    array<int, 256> numlist;
+    iota(numlist.begin(), numlist.end(), 0);
+
+    int position = 0;
+    int skip     = 0;
+
+    for (int i = 0; i < 64; ++i) {
+        for (auto len : len_vec) {
+            hash_step(numlist, position, skip, len);
+        }
+    }
+
+    vector<int> dence_hash;
+    for (int i = 0; i < 16; ++i) {
+        int dence = numlist[i * 16];
+        for (int j = 1; j < 16; ++j) {
+            dence ^= numlist[i * 16 + j];
+        }
+        dence_hash.push_back(dence);
+    }
+
+    string knot_hash;
+    string hexchars = "0123456789abcdef";
+    for (auto dence : dence_hash) {
+        knot_hash.push_back(hexchars[(dence & 0xf0) >> 4]);
+        knot_hash.push_back(hexchars[dence & 0x0f]);
+    }
+
+    println("{}", knot_hash);
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
