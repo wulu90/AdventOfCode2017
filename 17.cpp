3c0823f7460c79eec471430281008d51bedc8654
wulu <wulu90@outlook.com>
2025-02-28 15:10:28 +0800

    17-2

--- 17.cpp
+++ 17.cpp
@@ -9,19 +9,34 @@ void part1() {
     buffer.push_back(0);
     size_t pos = 0;
     for (int i = 1; i <= 2017; ++i) {
-        pos = (pos + input) % i;
-        if (pos == i - 1) {
+        pos = (pos + input) % i + 1;
+
+        if (pos == i) {
             buffer.push_back(i);
-            pos = i;
         } else {
-            pos += 1;
             buffer.insert(buffer.begin() + pos, i);
         }
     }
     println("{}", buffer[pos + 1]);
 }
 
+void part2() {
+    static const int input = 304;
+
+    size_t pos     = 0;
+    int after_zero = 0;
+    for (int i = 1; i <= 5000'0000; ++i) {
+        pos = (pos + input) % i + 1;
+        if (pos == 1) {
+            after_zero = i;
+        }
+    }
+
+    println("{}", after_zero);
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
