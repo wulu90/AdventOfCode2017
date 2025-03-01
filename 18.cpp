32e323377adbeab860af09f7f9432acdec1a2bb9
wulu <wulu90@outlook.com>
2025-03-01 17:36:41 +0800

    18-2

--- 18.cpp
+++ 18.cpp
@@ -1,6 +1,7 @@
 #include <charconv>
 #include <fstream>
 #include <map>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -77,7 +78,67 @@ void part1() {
     }
 }
 
+void run(const vector<string>& ins_vec, map<char, int64_t>& regs, size_t& off, queue<int64_t>& s_q, queue<int64_t>& r_q,
+         int64_t& snd_count) {
+    while (off < ins_vec.size()) {
+        auto& ins = ins_vec[off];
+        if (ins.starts_with("snd")) {
+            s_q.push(get_x(ins, regs));
+            ++snd_count;
+            ++off;
+        } else if (ins.starts_with("rcv")) {
+            if (r_q.empty()) {
+                break;
+            } else {
+                regs[ins[4]] = r_q.front();
+                r_q.pop();
+                ++off;
+            }
+        } else if (ins.starts_with("jgz")) {
+            if (get_x(ins, regs) > 0) {
+                off += get_y(ins, regs);
+            } else {
+                ++off;
+            }
+        } else {
+            set_add_mul_mod(ins, regs);
+            ++off;
+        }
+    }
+}
+
+void part2() {
+    ifstream input("input/input18");
+    vector<string> ins_vec;
+    for (string line; getline(input, line);) {
+        ins_vec.push_back(line);
+    }
+
+    map<char, int64_t> regs0, regs1;
+    regs0['p'] = 0;
+    regs1['p'] = 1;
+
+    size_t off0 = 0;
+    size_t off1 = 0;
+
+    queue<int64_t> q0;
+    queue<int64_t> q1;
+    int64_t snd_count0 = 0;
+    int64_t snd_count1 = 0;
+
+    while (true) {
+        run(ins_vec, regs0, off0, q1, q0, snd_count0);
+        run(ins_vec, regs1, off1, q0, q1, snd_count1);
+
+        if (q0.empty() && q1.empty()) {
+            println("{},{}", snd_count0, snd_count1);
+            break;
+        }
+    }
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
